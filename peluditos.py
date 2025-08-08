65d5933478fe6bcb7f2db370c94f8ae284b56fbb
Jonathan <jonathanlibrerosc1145@gmail.com>
2025-08-08 03:01:00 -0500

    Se ajustó el peluditos.py de la raíz para correrlo local

--- peluditos.py
+++ peluditos.py
@@ -1,4 +1,4 @@
-from routes.peluditos import app
+from peluditos import app
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
