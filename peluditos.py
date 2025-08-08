6ed0e7a6b6f685d593eafc2602a9073d7826a731
Jonathan <jonathanlibrerosc1145@gmail.com>
2025-08-07 23:10:58 -0500

    Se creó un Procfile y se ajustó peluditos.py para desplegar en Railway

--- peluditos.py
+++ peluditos.py
@@ -3,6 +3,7 @@ from random import randint
 from flask import Flask, redirect, render_template, request, session
 import mysql.connector
 import hashlib
+import os
 
 app = Flask(__name__)
 app.secret_key = str(randint(100000, 99999))
@@ -59,4 +60,5 @@ def logout():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
